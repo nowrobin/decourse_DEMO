265b77e8ed67756aa4392184f1149b0e3b23d91c
robin <nowrobin3@gmail.com>
2024-10-29 18:00:19 +0900

    week11 final

--- frontend/typescript/oop.js
+++ frontend/typescript/oop.js
@@ -1,15 +1,33 @@
 //직원 정보
 var Employee = /** @class */ (function () {
-    function Employee(empName, age, empJob) {
+    // private _empName: string;
+    // public age: number;
+    // protected empJob: string;
+    function Employee(_empName, _age, _empJob) {
         var _this = this;
+        this._empName = _empName;
+        this._age = _age;
+        this._empJob = _empJob;
         this.printEmp = function () {
             console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, " \uC9C1\uC5C5\uC740 ").concat(_this.empJob));
         };
-        this.empName = empName;
-        this.age = age;
-        this.empJob = empJob;
+        // this._empName = empName;
+        // this.age = age;
+        // this.empJob = empJob;
     }
+    Object.defineProperty(Employee.prototype, "empName", {
+        //getter
+        get: function () {
+            return this._empName;
+        },
+        set: function (name) {
+            this._empName = name;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
 var employee1 = new Employee("x", 20, "개발자");
+employee1.empName = "lee";
 employee1.printEmp();
--- frontend/typescript/oop.ts
+++ frontend/typescript/oop.ts
@@ -1,14 +1,27 @@
 //직원 정보
 
 class Employee {
-  empName: string;
-  age: number;
-  empJob: string;
-  constructor(empName: string, age: number, empJob: string) {
-    this.empName = empName;
-    this.age = age;
-    this.empJob = empJob;
+  // private _empName: string;
+  // public age: number;
+  // protected empJob: string;
+
+  constructor(
+    private _empName: string,
+    private _age: number,
+    private _empJob: string
+  ) {
+    // this._empName = empName;
+    // this.age = age;
+    // this.empJob = empJob;
   }
+  //getter
+  get empName() {
+    return this._empName;
+  }
+  set empName(name: string) {
+    this._empName = name;
+  }
+
   printEmp = (): void => {
     console.log(`${this.empName}의 나이는 ${this.age} 직업은 ${this.empJob}`);
   };
@@ -16,4 +29,5 @@ class Employee {
 
 let employee1 = new Employee("x", 20, "개발자");
 
+employee1.empName = "lee";
 employee1.printEmp();
